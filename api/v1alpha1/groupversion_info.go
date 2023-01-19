816efcd6fe1cd034e514ca4dcc48eb8344a8b902
Balazs Nemeth <bnemeth@redhat.com>
2023-01-19 08:48:58 +0100

    update golang to version 1.17
    Signed-off-by: Balazs Nemeth <bnemeth@redhat.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the dpu v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=dpu.openshift.io
+// +kubebuilder:object:generate=true
+// +groupName=dpu.openshift.io
 package v1alpha1
 
 import (
